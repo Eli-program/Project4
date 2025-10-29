a7793a39e64f88fab38da3e304cc89bc88f3478e
Elijah-Program <elijah.reid.bell@gmail.com>
2025-10-29 12:05:44 -0400

    e

--- Project4/Shipping.cpp
+++ Project4/Shipping.cpp
@@ -1,51 +1,44 @@
 #include <iostream>
 using namespace std;
 
+int main() {
+    int minWeight = 0;
+    int maxWeight = 20;
+    cout << "Enter package weight: ";
+    int packageWeight;
+    cin >> packageWeight;
 
-int main()
-{
-	int minWeight = >=0;
-	int maxWeight = 20;
-	cout << "Enter package weight: ";
-	int packageWeight;
-	cin >> packageWeight;
+    int minDistance = 10;
+    int maxDistance = 3000;
+    cout << "Enter distance to be shipped: ";
+    int distance;
+    cin >> distance;
 
-	int distance;
-	int maxDistance = 3000;
-	int minDistance = 10;
-	cout << "Enter distance to be shipped: ";
-	cin >> distance;
+    double rate;
 
-	int rate;
+    if (packageWeight > maxWeight || packageWeight <= minWeight) {
+        cout << "Invalid package weight." << endl;
+    }
+    else if (distance > maxDistance || distance < minDistance) {
+        cout << "Invalid distance." << endl;
+    }
+    else {
+        if (packageWeight <= 2) {
+            rate = 1.10;
+        }
+        else if (packageWeight <= 6) {
+            rate = 2.20;
+        }
+        else if (packageWeight <= 10) {
+            rate = 3.70;
+        }
+        else {
+            rate = 4.80;
+        }
 
-	if (packageWeight > maxWeight || packageWeight <= minWeight)
-	{
-		cout << "Invalid package weight." << endl;
-	}
-	else if (distance > maxDistance || distance < minDistance)
-	{
-		cout << "Invalid distance." << endl;
-	}
-	else
-	{
-		if (packageWeight <= 2)
-		{
-			rate = 1.10;
-		}
-		else if (packageWeight <= 6)
-		{
-			rate = 2.20;
-		}
-		else if (packageWeight <= 10)
-		{
-			rate = 3.70;
-		}
-		else
-		{
-			rate = 4.80;
-		}
-		double totalCost = rate * (distance / 500.0);
-		cout << "Total shipping cost: $" << totalCost << endl;
-	}
+        double totalCost = rate * (distance / 500.0);
+        cout << "Total shipping cost: $" << totalCost << endl;
+    }
 
+    return 0;
 }
